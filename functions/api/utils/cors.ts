729c5e90f6e3963c7d38bc7fe5720140d87cdeb8
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2025-12-01 11:11:42 -0300

    adding PATCH in access contorl allow methods

--- functions/api/utils/cors.ts
+++ functions/api/utils/cors.ts
@@ -1,7 +1,9 @@
 export function corsHeaders(origin: string | null) {
   return {
     "Access-Control-Allow-Origin": origin ?? "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "authorization, content-type, x-client-info, apikey",
+    // ⬇⬇ IMPORTANT: include PATCH (and usually PUT/DELETE too)
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "authorization, content-type, x-client-info, apikey",
   };
 }
